6cf4c4f5912346fa0c00130a2fb81cd022d47ee9
Flavio Barbosa <flavioramone@gmail.com>
2022-11-17 05:02:58 -0300

    create apis

---
+++ api/src/app/useCases/orders/listOrders.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+
+import { Order } from '../../models/Order';
+
+export async function listOrders(req: Request, res: Response) {
+  try {
+    const categories = await Order.find()
+      .sort({createdAt: 1})
+      .populate('products.product');
+
+    res.json(categories);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+}
