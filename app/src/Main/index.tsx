f6df6a0a320d28e04e7f63d4bffb9b0f548c3853
Flavio Barbosa <flavioramone@gmail.com>
2022-12-12 16:30:06 -0300

    fix git dir structure

---
+++ app/src/Main/index.tsx
@@ -0,0 +1,26 @@
+import { CategoriesContainer, Container, Footer, FooterContainer, MenuContainer } from './styles';
+
+import { Header } from '../components/Header';
+import { Categories } from '../components/Categories';
+import { Menu } from '../components/Menu';
+
+export function Main() {
+  return (
+    <>
+      <Container>
+        <Header />
+        <CategoriesContainer>
+          <Categories />
+        </CategoriesContainer>
+
+        <MenuContainer>
+          <Menu />
+        </MenuContainer>
+
+      </Container>
+      <Footer>
+        <FooterContainer></FooterContainer>
+      </Footer>
+    </>
+  );
+}
