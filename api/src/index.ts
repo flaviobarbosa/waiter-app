3207b2000ffbc03cefdf53ba3ce2b59532ab7322
Flavio Barbosa <flavio.barbosa@dataprev.gov.br>
2022-12-20 14:26:21 -0300

    add cors settings

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,19 +1,27 @@
-import path from "node:path";
+import path from 'node:path';
 
-import express from "express";
-import mongoose from "mongoose";
+import express from 'express';
+import mongoose from 'mongoose';
 
-import { router } from "./routes";
+import { router } from './routes';
 
 mongoose
-  .connect("mongodb://localhost:27017")
+  .connect('mongodb://localhost:27017')
   .then(() => {
     const app = express();
     const port = 3001;
 
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+
+      next();
+    });
+
     app.use(
-      "/uploads",
-      express.static(path.resolve(__dirname, "..", "uploads"))
+      '/uploads',
+      express.static(path.resolve(__dirname, '..', 'uploads'))
     );
     app.use(express.json());
     app.use(router);
@@ -22,4 +30,4 @@ mongoose
       console.log(`🚀 Server is running on http://localhost:${port}`);
     });
   })
-  .catch(() => console.error("erro ao conectar no mongo"));
+  .catch(() => console.error('erro ao conectar no mongo'));
