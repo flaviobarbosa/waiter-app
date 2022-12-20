d0c613c546846227bcc5a4f591080a0dec1de860
Flavio Barbosa <flavio.barbosa@dataprev.gov.br>
2022-12-20 16:16:41 -0300

    add socket for orders

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,24 @@
 import path from 'node:path';
 
+import http from 'node:http';
+
 import express from 'express';
 import mongoose from 'mongoose';
 
+import { Server } from 'socket.io';
+
 import { router } from './routes';
+
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
 
 mongoose
   .connect('mongodb://localhost:27017')
   .then(() => {
-    const app = express();
     const port = 3001;
+
+
 
     app.use((req, res, next) => {
       res.setHeader('Access-Control-Allow-Origin', '*');
@@ -26,7 +35,7 @@ mongoose
     app.use(express.json());
     app.use(router);
 
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`🚀 Server is running on http://localhost:${port}`);
     });
   })
